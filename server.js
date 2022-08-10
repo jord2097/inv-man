2d8de12135c7140d223554b116285690db0d49df
jord2097 <100094374+jord2097@users.noreply.github.com>
2022-08-10 23:50:28 +0100

    models, routes

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import router from './router'
+const uri = "" // MongoDB URI
 
 dotenv.config()
 const port = process.env.PORT || 3000
@@ -13,19 +16,24 @@ const app = express()
 app.use(helmet())
 app.use(cors())
 app.use(morgan('combined'))
+app.use(router)
 app.use(bodyParser.json()) // parses and extracts a request's JSON body to req.body
 app.disable("x-powered-by") // keeps technologies used private
 
 // START
+let server
+let db
 try {
     // SERVER START
     server = await app.listen(PORT)
     // DB START
-
+    db = await mongoose.connection
 }
 catch (err) {
     console.log(err)
 }
 finally {
     console.log(`API listening on http://localhost:%${port}`)
+    console.log(`Connected to DB`)
+    
 }
